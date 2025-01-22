fd0f43c85c3ed64d0f1bd80c35033afb53584ff3
Gilbert Hawkins Winja <104626373+hawkinswinja@users.noreply.github.com>
2025-01-23 02:21:22 +0300

    fix failing imagetag value

--- utils/buildContainerTemplate.sh
+++ utils/buildContainerTemplate.sh
@@ -9,6 +9,7 @@ while getopts "f:g:s:u:r:t:d:" option;
         r ) REPOSITORY=${OPTARG};;
         t ) TASKNAME=${OPTARG};;
         d ) DOCKER_FILE=${OPTARG};;
+        m ) imageTag=${OPTARG};;
     esac
 done
 echo $SRC_FOLDER
@@ -18,15 +19,16 @@ echo $AZ_ACR_NAME
 echo $REPOSITORY
 echo $TASKNAME
 echo $DOCKER_FILE
+echo $imageTag
 
 set -euxo pipefail  # fail on error
   
 # Generate an tag with a reproducible checksum of all files in . by doing a checksum of all files
 # in alphabetical order, then another checksum of their names and checksums.
 # Running this command on windows-based infrastructure may return a different result due to CRLF
-pushd $GITHUB_WORKSPACE/$SRC_FOLDER/$SOURCE_LOCATION
-imageTag=$(git log -n 1 --format="%H" -- ".")
-popd
+# pushd $GITHUB_WORKSPACE/$SRC_FOLDER/$SOURCE_LOCATION
+# imageTag=$(git log -n 1 --format="%H" -- ".")
+# popd
   
 # If the image with the generated tag doesn't already exist, build it.
 if ! az acr repository show -n $AZ_ACR_NAME --image "$REPOSITORY:$imageTag" -o table; then
